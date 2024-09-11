35f2532300a4e1556fbe4581cb42358cec21cdfd
fsikali <flemmingsikali499@gmail.com>
2024-09-11 16:34:28 +0300

    modified: src/index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-import React  from "react";
+import react from 'react'; 
 import ReactDOM from 'react-dom/client'; 
 
-const myFirstElement = <h1>Hello React!</h1> 
+const myArray = ['apple', 'banana', 'orange']; 
+const myList = myArray.map((item) => <p> {item} </p>)
 
 const root = ReactDOM.createRoot(document.getElementById('root')); 
-root.render(myFirstElement);
+root.render(myList);  
+
+// ReactDOM.render(myList, document.getElementById('root'));
+
 
 
