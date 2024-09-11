a7f00555ff74def64baf88f73eac2dc50966f864
fsikali <flemmingsikali499@gmail.com>
2024-09-11 17:28:48 +0300

    new file: src/Tutorials/app_b.js

---
+++ src/Tutorials/app_b.js
@@ -0,0 +1,27 @@
+import react from 'react'; 
+import ReactDOM from 'react-dom/client';  
+
+// Destructuring deeply nested objects  
+
+const vehicleOne = { 
+    brand: 'Ford', 
+    model: 'Mustang', 
+    type: 'car', 
+    year: '2021', 
+    color: 'red', 
+    registration: { 
+        country: 'Kenya', 
+        county: 'Nairobi', 
+        sub_county: 'Embakasi',
+    } 
+}
+
+myVehicle(vehicleOne) 
+
+function myVehicle({model, registration: { country } }) {  
+    const message = 'My ' + model + ' is registered in ' + country + ' . '; 
+
+    const root = ReactDOM.createRoot(document.getElementById('root')); 
+    root.render(message);
+} 
+
